93d02d40687722c0c13825e8f839c91fd3a1e2c7
Julian Cataldo <contact@juliancataldo.com>
2022-06-13 08:35:14 +0200

    refactor: rename 'fetch' to 'cms'

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     // Custom fetch
-    '~/plugins/fetch.ts',
+    '~/plugins/cms.ts',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
