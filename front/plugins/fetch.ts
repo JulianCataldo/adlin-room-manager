4165267df90a51ef17bae9f4b2f480e3040884ba
Julian Cataldo <contact@juliancataldo.com>
2022-06-13 08:34:18 +0200

    feat: add post method to cms data retriever

--- front/plugins/fetch.ts
+++ front/plugins/fetch.ts
@@ -2,28 +2,28 @@ import { Context, Plugin } from '@nuxt/types';
 
 declare module '@nuxt/types' {
   interface Context {
-    $fetch(req: Context['req'], apiPath: string): Promise<null | any>;
+    $cms(req: Context['req'], apiPath: string): Promise<null | any>;
   }
 }
 
-const fetch: Plugin = (context) => {
-  context.$fetch = async (req: Context['req'], apiPath: string) => {
-    console.log(`fetching ${apiPath}…`);
+const cms: Plugin = (context) => {
+  context.$cms = async (req: Context['req'], apiPath: string) => {
+    console.log(`Accessing CMS ${apiPath}…`);
 
-    // const url = new URL(apiPath, process.env.PRIVATE_CMS).toString();
-    // const Authorization = `Bearer ${process.env.PRIVATE_CMS_TOKEN}`;
-    const url = new URL(
-      ['api', apiPath].join('/'),
-      req?.headers?.referer || location?.href,
-    ).toString();
+    let baseUrl;
+    if (process.client) {
+      baseUrl = location?.href;
+    } else if (process.server) {
+      const protocol = process.env.TLS_ACTIVE ? 'https://' : 'http://';
+      baseUrl = protocol + req?.headers?.host;
+    }
+
+    const url = new URL(['api', apiPath].join('/'), baseUrl).toString();
 
     console.log({ apiUrl: url });
 
     const data = await context
-      .$axios(
-        url,
-        //  { headers: { Authorization } }
-      )
+      .$axios(url)
       .catch((error) => console.log(error))
       .then((response) => {
         if (response) {
@@ -37,33 +37,4 @@ const fetch: Plugin = (context) => {
   };
 };
 
-export default fetch;
-
-// export type Fetch = (req: any,apiPath: string) => Promise<null|any>;
-
-// export default ({ $axios }: NuxtApp, inject: Inject) => {
-//   async function fetch(req: Http.re, apiPath: string) : Promise<null|any> {
-//     console.log(req.);
-//     console.log(`fetching ${apiPath}…` );
-
-//     // const url = new URL(apiPath, process.env.PRIVATE_CMS).toString();
-// //     const url = new URL(apiPath, 'http://'+req.headers.host).toString();
-// //     // const Authorization = `Bearer ${process.env.PRIVATE_CMS_TOKEN}`;
-// // console.log({apiUrl: url});
-// //     const data = await $axios(url,
-// //       //  { headers: { Authorization } }
-// //        )
-// //       .catch((error) => console.log({ error }))
-// //       .then((response) => {
-// //         if (response) {
-// //           console.log('data found');
-// //           return response.data;
-// //         }
-// //         console.log('no data found !');
-// //         return null;
-// //       });
-//     return 'data';
-//   };
-
-//   inject('fetch', fetch);
-// };
+export default cms;
